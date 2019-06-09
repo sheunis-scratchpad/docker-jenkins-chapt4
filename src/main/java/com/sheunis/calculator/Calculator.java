d9d46c12e23ffba22bf20e3bf52f21b127cdfa85
Sebastiaan Heunis <sheunis@gmail.com>
2019-06-09 17:14:18 +0100

    Adding checkstyle

--- src/main/java/com/sheunis/calculator/Calculator.java
+++ src/main/java/com/sheunis/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package com.sheunis.calculator;
 import org.springframework.stereotype.Service;
 
+/**
+ * Calculator service
+ */
 @Service
 public class Calculator {
     int sum(int a, int b) {
--- src/main/java/com/sheunis/calculator/CalculatorController.java
+++ src/main/java/com/sheunis/calculator/CalculatorController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Calculator controller
+ */
 @RestController
 class CalculatorController {
     @Autowired
